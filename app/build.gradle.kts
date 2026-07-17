383fbf95db0d8a47681839e65338744d08a8051d
VHanma <morgandeavon@gmail.com>
2026-07-17 15:46:18 -0500

    Version Aethena Qwen3.5 4B build 0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aethena.agent"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         ndk.abiFilters += "arm64-v8a"
